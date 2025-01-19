a3abf13fef932a8d34f60ab8583a239dbb7d990d
Omar Faruque Tuhin <nadimtuhin@gmail.com>
2025-01-19 18:02:50 +0600

    Update love modal show delay from 30 seconds to 15 seconds for improved user experience

--- src/app/config/modals.ts
+++ src/app/config/modals.ts
@@ -1,6 +1,6 @@
 export const MODAL_CONFIG = {
   love: {
-    showDelay: 30000, // 30 seconds in milliseconds
+    showDelay: 15000, // 15 seconds in milliseconds
     storageKey: 'love-modal-shown'
   }
 } as const
