22a6f33af88cf3d8425a8242f4f2c885bb727249
Omar Faruque Tuhin <nadimtuhin@gmail.com>
2025-01-19 16:37:28 +0600

    Update layout.tsx to change outbound link tracking script URL from HTTP to HTTPS for improved security and reliability.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script defer data-domain="nadimtuhin.com" src="http://null.app.nadimtuhin.com/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+        <script defer data-domain="nadimtuhin.com" src="https://null.app.nadimtuhin.com/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
       </head>
       <body className="bg-secondary min-h-screen">
         <Analytics />
